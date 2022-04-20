aa0ca1d268f03baf8ee7f7210b1c1c559fc1b3f8
DianaMagallanes <dianakarina2003@example.com>
2022-04-19 18:57:40 -0700

    Creando archivo index Instancias de Class MissionCommander

---
+++ index.js
@@ -0,0 +1,10 @@
+const MissionCommander = require('./app/missionCommander');
+const carlo = new MissionCommander("Carlo")
+console.log(carlo.name)
+
+const diana = new MissionCommander("Diana")
+console.log(diana.name)
+const karina = new MissionCommander("Karina")
+console.log(karina.name)
+const clara = new MissionCommander("Clara")
+console.log(clara.name)
